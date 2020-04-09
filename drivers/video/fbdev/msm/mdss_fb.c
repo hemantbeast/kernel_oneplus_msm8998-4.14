b8b3e4dba636b4e4386f39022d516c553721c734
shxyke <shxyke@gmail.com>
2020-04-09 19:59:47 +0800

    mdss: Introduce the flicker free driver
    This driver provides exposure adjustment function by Qcom
    DSPP API without change panel hardware brightness to avoid
    PWM flicker on OLED devices, thanks to Sony Mobile for the
    method of setting PCC.
    
    Your device must support ELVSS off function except HBM mode.
    
    Use mdss_panel_calc_backlight to remap brightness with
    hardware and DSPP.And this driver will create 2 files in the
    proc directory, you could change the fliker free status with them.
    echo 1 to /proc/flicker_free/flicker_free to enable, echo 0 to disable.
    echo the value of ELVSS to /proc/flicker_free/min_brightness to set
    it, you can also use command "cat" to get the status or the current
    ELVSS value.
    
    Signed-off-by: shxyke <shxyke@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -1996,7 +1996,6 @@ void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl)
 		} else {
 			if (mfd->bl_level != bkl_lvl)
 				bl_notify_needed = true;
-			pr_debug("backlight sent to panel :%d\n", temp);
 
 			if (mfd->mdp.is_twm_en)
 				twm_en = mfd->mdp.is_twm_en();
@@ -2004,7 +2003,15 @@ void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl)
 			if (twm_en) {
 				pr_info("TWM Enabled skip backlight update\n");
 			} else {
+			#ifdef CONFIG_FLICKER_FREE
+				ff_mfd_copy = mfd;
+				ff_bkl_lvl_cpy = temp;
+				pr_debug("backlight sent to panel :%d\n", mdss_panel_calc_backlight(temp));
+				pdata->set_backlight(pdata, mdss_panel_calc_backlight(temp));
+			#else
+				pr_debug("backlight sent to panel :%d\n", temp);
 				pdata->set_backlight(pdata, temp);
+			#endif
 				mfd->bl_level = bkl_lvl;
 				mfd->bl_level_scaled = temp;
 			}
@@ -2017,6 +2024,17 @@ void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl)
 				NOTIFY_TYPE_BL_UPDATE);
 	}
 }
+
+#ifdef CONFIG_FLICKER_FREE
+struct msm_fb_data_type *get_mfd_copy(void)
+{
+	return ff_mfd_copy;
+}
+
+u32 get_bkl_lvl(void){
+	return ff_bkl_lvl_cpy;
+}
+#endif
 
 void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 {
@@ -2039,7 +2057,13 @@ void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 				mdss_fb_bl_update_notify(mfd,
 					NOTIFY_TYPE_BL_AD_ATTEN_UPDATE);
 			mdss_fb_bl_update_notify(mfd, NOTIFY_TYPE_BL_UPDATE);
+		#ifdef CONFIG_FLICKER_FREE
+			ff_mfd_copy = mfd;
+			ff_bkl_lvl_cpy = temp;
+			pdata->set_backlight(pdata, mdss_panel_calc_backlight(temp));
+		#else
 			pdata->set_backlight(pdata, temp);
+		#endif
 			mfd->bl_level_scaled = mfd->unset_bl_level;
 			mfd->allow_bl_update = true;
 		}
