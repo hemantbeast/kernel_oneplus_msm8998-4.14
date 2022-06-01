18729f21f1d0c6220c79f9ff5c01953d391ae030
ederekun <sedrickvince@gmail.com>
2022-06-01 22:01:09 +0300

    qdsp6v2: msm-pcm-routing-v2: Disable custom PCM topologies
    This should significantly reduce audio post-processing. Comment out routing
    to custom topologies' de/init functions.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -195,6 +195,7 @@ static bool msm_is_resample_needed(int input_sr, int output_sr)
 static void msm_pcm_routing_cfg_pp(int port_id, int copp_idx, int topology,
 				   int channels)
 {
+#if 0 /* Disable custom PCM topologies */
 	int rc = 0;
 
 	switch (topology) {
@@ -240,10 +241,12 @@ static void msm_pcm_routing_cfg_pp(int port_id, int copp_idx, int topology,
 		/* custom topology specific feature param handlers */
 		break;
 	}
+#endif
 }
 
 static void msm_pcm_routing_deinit_pp(int port_id, int topology)
 {
+#if 0 /* Disable custom PCM topologies */
 	switch (topology) {
 	case SRS_TRUMEDIA_TOPOLOGY_ID:
 		pr_debug("%s: SRS_TRUMEDIA_TOPOLOGY_ID\n", __func__);
@@ -271,6 +274,7 @@ static void msm_pcm_routing_deinit_pp(int port_id, int topology)
 		/* custom topology specific feature deinit handlers */
 		break;
 	}
+#endif
 }
 
 static void msm_pcm_routng_cfg_matrix_map_pp(struct route_payload payload,
